04035fd5c3b58a612296dc991cd5be55cd3aa436
Terrell Douglas <terrelloydouglas@yahoo.com>
2019-11-14 13:40:18 -0800

    mass changes, got rewriting to/from db

--- server.js
+++ server.js
@@ -18,107 +18,84 @@ const client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
 client.on('error', err => console.error(err));
 
-app.get('/', newSearch);
-// app.post('/searches', searchForBooks);
-
 // API routes
-app.get('/', getBooks) //get all books
+app.get('/', getBooks); //get all books
 app.get('/books/:book_id', updateBook); //get one book
-// app.get('/add', showForm); // show form to add a book
 app.get('/add', addBook); // create a new book
+app.get('/searches/new', newSearch);
 app.post('/searches', searchForBooks);
 app.post('/books', createBook);
 
 
-function newSearch(request, response) {
-  response.render('pages/index');
+function newSearch(req, res) {
+  res.render('pages/searches/new');
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 function createBook(req, res) {
-  let { title, description, category, contact, status } = req.body;
-  let SQL = 'INSERT INTO books ( title, description, category, contact, status) VALUES ($1, $2, $3, $4, $5);';
-  let safeValues = [title, description, category, contact, status];
-  console.log('in createBooks');
+  let { title, author, etag, image_url, description, bookshelf } = req.body;
+  let SQL = 'INSERT INTO books (title, author, etag, image_url, description, bookshelf) VALUES ($1, $2, $3, $4, $5, $6);';
+
+  let safeValues = [title, author, etag, image_url, description, bookshelf];
+
   client.query(SQL, safeValues)
     .then(() => {
-      SQL = 'SELECT * FROM books WHERE isbn = $1;';
-      safeValues = [req.body.isbn]
+      SQL = 'SELECT * FROM books WHERE etag = $1;';
+      safeValues = [req.body.etag]
 
-      client.query(SQL, safeValues)
-        .then((result => {
-          res.redirect('/books/${result.rows[0].id')
+      client.query(sql, safeValues)
+        .then((result) => {
+          response.redirect(`/books/${result.rows[0].id}`)
         })
-        )
     })
 }
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-function searchForBooks(request, response) {
-  // console.log(request.body);
-  const userSearch = request.body.search[0];
-  const typeOfSearch = request.body.search[1];
-  console.log('in searchForBooks');
-  // https://www.googleapis.com/books/v1/volumes?q=moby+intitle:title
+function searchForBooks(req, res) {
   let url = `https://www.googleapis.com/books/v1/volumes?q=`;
 
-  if (typeOfSearch === 'title') {
-    url += `+intitle:${userSearch}`
-  }
-
-  if (typeOfSearch === 'author') {
-    url += `+inauthor:${userSearch}`
-  }
+  if (req.body.search[1] === 'title') { url += `+intitle:${req.body.search[0]}`; }
+  if (req.body.search[1] === 'author') { url += `+inauthor:${req.body.search[0]}`; }
 
   superagent.get(url)
-    .then(results => results.body.items.map(book => new Book(book.volumeInfo)))
-    .then(bookArr => response.render('pages/searches/show', { searchResults: bookArr }))
-    .catch(results => response.render('pages/searches/error'));
-
+    .then(apiResponse => apiResponse.body.items.map(bookResult => new Book(bookResult)))
+    .then(bookArr => res.render('pages/searches/show', { arrItems: bookArr }))
+    .catch(() => {
+      res.render('pages/error');
+    })
 }
 
 let bookArr = [];
 
 //Constructor Function
 function Book(bookObj) {
+  this. id =bookObj.id;
+  this.title = bookObj.volumeInfo.title || 'No book title found';
+  this.authors = bookObj.volumeInfo.authors || 'No author';
+  this.description = bookObj.volumeInfo.description || 'No description defined';
   this.etag = bookObj.etag;
   this.image = bookObj.imageLinks.thumbnail || 'http://placehold.it/300x300';
-  this.title = bookObj.title || 'No book title found';
-  this.authors = bookObj.authors || 'No author';
-  this.description = bookObj.description || 'No description defined';
-  this.isbn = bookObj.industryIdentifiers[0].identifier|| 'No ISBN';
+  // this.image = `https://books.google.com/books/content?id=${id}&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api`;
   bookArr.push(this);
 }
 
-Book.prototype.writeToDB = function() {
-  const SQL = 'INSERT INTO books (title, author, description, image_url, isbn) VALUES ($1, $2, $3, $4, $5) ON CONFLICT DO NOTHING RETURNING id;';
-  const values = [this.title, this.authors, this.description, this.image, this.isbn];
-  console.log('in writeToDB',this.title);
-  return client.query(SQL, values);
-};
-
 function getBooks(req, res) {
   let SQL = 'SELECT * FROM books;';
-  console.log('in getBooks');
   return client.query(SQL)
-    .then(results => res.render('index', { results: results.rows }))
-    .catch(err => console.error(err));
+    .then(results => res.render('pages/index', { results: results.rows }))
+    .catch(() => {
+      res.render('pages/error');
+    })
 }
 
 function updateBook(req, res) {
   let SQL = 'SELECT * FROM books WHERE id=$1;';
   let values = [req.params.book_id];
-  console.log('in updateBooks');
+
   return client.query(SQL, values)
     .then(result => {
-      return res.render('pages/detail-view', { book: result.rows[0] });
+      return res.render('pages/books/show', { book: result.rows[0] });
     })
     .catch(err => console.error(err));
-}
-
-function showForm(req, res) {
-  res.render('./pages/add-view');
 }
 
 function addBook(req, res) {
@@ -132,6 +109,4 @@ function addBook(req, res) {
     .catch(err => console.error(err));
 }
 
-app.listen(PORT, () => {
-  console.log(`Listening on ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
